ef035a2ca2776a0ef1a64c94af4c29213837cb98
Keren Trajtenberg <keren.trajtenberg@post.runi.ac.il>
2024-03-18 17:31:34 +0200

    changes to vars file

--- Terraform/deploy-fsx-ontap/standalone-module/variables.tf
+++ Terraform/deploy-fsx-ontap/standalone-module/variables.tf
@@ -21,20 +21,20 @@ variable "fsx_subnets" {
 
 variable "fsx_capacity_size_gb" {
    description = "The storage capacity (GiB) of the FSxN file system. Valid values between 1024 and 196608"
-   type = number
-   default = 1024
+   type        = number
+   default     = 1024
 }
 
 variable "fsx_deploy_type" {
    description = "The filesystem deployment type. Supports MULTI_AZ_1 and SINGLE_AZ_1"
-   type = string 
-   default = "MULTI_AZ_1"
+   type        = string 
+   default     = "MULTI_AZ_1"
 }
        
 variable "fsx_tput_in_MBps" {
    description = "The throughput capacity (in MBps) for the file system. Valid values are 128, 256, 512, 1024, 2048, and 4096."
-   type = number
-   default = 256
+   type        = number
+   default     = 256
 }
 
 variable "fsx_admin_password" {
@@ -45,19 +45,19 @@ variable "fsx_admin_password" {
 
 variable "svm_name" {
    description = "The name of the Storage Virtual Machine"
-   type = string
-   default = "first_svm"
+   type        = string
+   default     = "first_svm"
 }
 
 variable "vol_info" {
    description = "Details for the volume creation"
-   type = map
+   type = map(any)
    default = {
-     "vol_name" = "vol1"
-     "junction_path" = "/vol1"
-	  "size_mg" = 1024
-	  "efficiency" = true
+     "vol_name"         = "vol1"
+     "junction_path"    = "/vol1"
+	  "size_mg"          = 1024
+	  "efficiency"       = true
 	  "tier_policy_name" = "AUTO"
-	  "cooling_period" = 31
+	  "cooling_period"   = 31
    }
 }
