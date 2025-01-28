8054e8e317b6961df8280f1ca969b75b9f1016af
Keith Cantrell <kcantrel@gmail.com>
2025-01-28 15:54:00 -0600

    Move the contents of the scripts/iscsi.sh file as an HEREDOC to remove the depenecy of a separate file.

--- EKS/FSxN-as-PVC-for-EKS/terraform/eks-cluster.tf
+++ EKS/FSxN-as-PVC-for-EKS/terraform/eks-cluster.tf
@@ -133,6 +133,19 @@ data "cloudinit_config" "cloudinit" {
 
   part {
     content_type = "text/x-shellscript"
-    content      = file("scripts/iscsi.sh")
+    content      = <<EOT
+#!/bin/bash
+sudo yum install -y lsscsi iscsi-initiator-utils sg3_utils device-mapper-multipath
+rpm -q iscsi-initiator-utils
+sudo sed -i 's/^\(node.session.scan\).*/\1 = manual/' /etc/iscsi/iscsid.conf
+cat /etc/iscsi/initiatorname.iscsi
+sudo mpathconf --enable --with_multipathd y --find_multipaths n
+#
+# Blacklist any EBS volume since they don't support them!
+sed -i -e '/^blacklist {/,/^}/{/^}/i\    device {\n        vendor "NVME"\n        product "Amazon Elastic Block Store"\n    }\n' -e '}' /etc/multipath.conf
+sudo systemctl restart multipathd
+sudo systemctl enable --now iscsid multipathd
+sudo systemctl enable --now iscsi
+EOT
   }
 }
