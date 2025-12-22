c135ec5a892e73ea569145ef09c74ff8566dfe4d
Keith Cantrell <kcantrel@gmail.com>
2025-12-22 10:39:22 -0600

    Removed the GenAI-ChatBot-application-sample since it is being maintained else where.

--- AI/GenAI-ChatBot-application-sample/src/app/[locale]/auth/constants.ts
+++
@@ -1,9 +0,0 @@
-const prod = {
-    endpointAfterRegistration: "/registerconfirm"
-};
-
-const dev = {
-    endpointAfterRegistration: "/registerconfirm"
-};
-
-export const config = dev //process.env.NODE_ENV === "development" ? dev : prod;
--- AI/GenAI-ChatBot-application-sample/src/app/[locale]/page.tsx
+++
@@ -1,5 +0,0 @@
-import Home from "./home";
-
-export default function Page() {
-  return <Home />;
-}
